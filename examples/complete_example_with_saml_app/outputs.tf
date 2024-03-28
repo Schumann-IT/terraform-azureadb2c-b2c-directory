5d6b13df84888dcf2bfee8d7246b3460d23913af
Jan Schumann <js@schumann-it.com>
2024-03-28 11:08:01 +0100

    fmt

--- examples/complete_example_with_saml_app/outputs.tf
+++ examples/complete_example_with_saml_app/outputs.tf
@@ -1,13 +1,13 @@
 output "azure_b2c_cli_settings" {
-  value = <<-EOT
-    AADCommonClientID:                     ${module.b2c.extension_app_registration_application_id}
-    AADCommonObjectID:                     ${module.b2c.extension_app_registration_object_id}
-    IdentityExperienceFrameworkAppId:      ${module.b2c.identity_experience_framework_application_id}
-    ProxyIdentityExperienceFrameworkAppId: ${module.b2c.proxy_identity_experience_framework_application_id}
-    SamlCertContainerID:                   ${module.b2c.custom_certificates[0]}
-    TokenEncryptionKeyContainerID:         ${module.b2c.identity_experience_framework_encryption_key_id}
-    TokenSigningKeyContainerID:            ${module.b2c.identity_experience_framework_signing_key_id}
-EOT
+  value = yamlencode({
+    AADCommonClientID                     = module.b2c.extension_app_registration_application_id
+    AADCommonObjectID                     = module.b2c.extension_app_registration_object_id
+    IdentityExperienceFrameworkAppId      = module.b2c.identity_experience_framework_application_id
+    ProxyIdentityExperienceFrameworkAppId = module.b2c.proxy_identity_experience_framework_application_id
+    SamlCertContainerID                   = module.b2c.custom_certificates[0]
+    TokenEncryptionKeyContainerID         = module.b2c.identity_experience_framework_encryption_key_id
+    TokenSigningKeyContainerID            = module.b2c.identity_experience_framework_signing_key_id
+  })
 }
 
 output "localizations" {
