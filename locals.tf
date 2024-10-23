33b7f48a5d9ae3140eea95e2e9fdb998a1ebc69a
Jan Schumann <js@schumann-it.com>
2024-10-23 22:03:13 +0200

    feat(custom-app): add secret management

--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
 locals {
   tenant_name = replace(var.domain_name, ".onmicrosoft.com", "")
+
+  storage_account = var.template_storage.manage == true && length(data.azurerm_storage_account.template_storage) > 0 ? data.azurerm_storage_account.template_storage[0] : azurerm_storage_account.template_storage[0]
 }
